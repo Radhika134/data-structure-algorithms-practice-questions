742fb8b531324d11b45520c2748b19bf7738155a
Radhika134 <radhikaagg134@gmail.com>
2026-06-25 14:50:41 +0530

    Time: 0 ms (100%), Space: 8.5 MB (83.41%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+
+       int rev = 0;
+        while(x)
+        {
+            int rem = x%10;
+
+            if( rev > INT_MAX / 10 || rev == INT_MAX && rev > 7) return 0;
+            if(rev < INT_MIN / 10 || rev == INT_MIN && rev < -8) return 0;
+            
+            rev = rev*10 + rem;
+            x = x/10;
+        }
+        return rev;
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
