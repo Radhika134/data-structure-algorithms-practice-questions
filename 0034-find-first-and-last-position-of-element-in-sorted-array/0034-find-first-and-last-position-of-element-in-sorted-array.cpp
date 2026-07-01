85ff776de6cab8033945e4c5cf2a2982e0c90e03
Radhika134 <radhikaagg134@gmail.com>
2026-07-01 13:18:27 +0530

    Time: 0 ms (100%), Space: 17.7 MB (19.35%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,12 +1,62 @@
 class Solution {
 public:
+int firstOcc(vector<int> & nums,int target, int n)
+{
+    int low = 0;
+    int high = n-1;
+    int start = -1;
+    while(low<=high)
+    {
+        int mid = low+((high-low)/2);
+        if(nums[mid] == target)
+        {
+            start = mid;
+            high = mid-1;
+
+        }
+        else if(nums[mid]>target)
+        {
+            high = mid-1;
+
+        }
+        else low = mid+1;
+    }
+    return start;
+}
+
+int lastOcc(vector<int> & nums,int target, int n)
+{
+    int low = 0;
+    int high = n-1;
+    int end = -1;
+    while(low<=high)
+    {
+        int mid = low+((high-low)/2);
+        if(nums[mid] == target)
+        {
+            end = mid;
+            low = mid+1;
+
+        }
+        else if(nums[mid]>target)
+        {
+            high = mid-1;
+
+        }
+        else low = mid+1;
+    }
+    return end;
+}
+
     vector<int> searchRange(vector<int>& nums, int target) {
         int n = nums.size();
-        int lb = lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-        int up = upper_bound(nums.begin(),nums.end(),target)-nums.begin();
-
-        if(lb == n || nums[lb] != target) return {-1,-1};
-        return {lb,up-1};
+        int first = firstOcc(nums,target,n);
+        if(first == -1)
+        {
+            return {-1,-1};
+        }
+        int last = lastOcc(nums,target,n);
+        return{first,last};
         
     }
 };
