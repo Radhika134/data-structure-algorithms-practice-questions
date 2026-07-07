18d787d7f7bbcac38c9ce0dc913b3052705c6d47
Radhika134 <radhikaagg134@gmail.com>
2026-07-07 10:42:00 +0530

    Time: 0 ms (100%), Space: 8.8 MB (10.62%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    bool rotateK(string s,string goal, int k)
-    {
+    bool rotateK(string s, string goal, int k) {
         int n = s.size();
         vector<char> ans(n);
         int j = 0;
@@ -10,7 +9,6 @@ public:
             j++;
         }
         string result(ans.begin(), ans.end());
-
         return result == s;
     }
     bool rotateString(string s, string goal) {
@@ -20,15 +18,20 @@ public:
         while (i < goal.size()) {
             if (s[0] == goal[i]) {
                 k = goal.size() - i;
-                
             }
-            if(rotateK(s,goal, k))
+            if (rotateK(s, goal, k))
                 return true;
             i++;
         }
-        
-        
-        return rotateK(s,goal,k);
-        
+
+        return rotateK(s, goal, k);
     }
+
+
+
+    // bool rotateString(string s, string goal) {
+    // if (s.size() != goal.size()) return false;
+    // string doubled = s + s;
+    // return (int)doubled.find(goal) != -1;
+
 };
