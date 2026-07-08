b7d7aeaab874e36e18b4b1a0040167d5e7fd3c78
Radhika134 <radhikaagg134@gmail.com>
2026-07-08 14:52:59 +0530

    Time: 0 ms (100%), Space: 8.2 MB (50.79%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -3,14 +3,13 @@ public:
     bool detectCapitalUse(string word) {
         int capCount = 0;
 
-        for(int i = 0 ; i< word.length(); i++)
-        {
-            if(isupper(word[i]))
-            {
+        for (int i = 0; i < word.length(); i++) {
+            if (isupper(word[i])) {
                 capCount++;
             }
         }
 
-        return (capCount == 0) || (capCount == word.length()) || (capCount == 1 && isupper(word[0]));
+        return (capCount == 0) || (capCount == word.length()) ||
+               (capCount == 1 && isupper(word[0]));
     }
 };
