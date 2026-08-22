cdbad2f3039296e5bf074c10629aea229a634830
Radhika134 <radhikaagg134@gmail.com>
2026-08-22 23:50:17 +0530

    Time: 3 ms (86.03%), Space: 43 MB (85.88%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+
+        for(int i = 0;i<s.length(); i++)
+        {
+            char ch = s.charAt(i);
+            if(ch == '{' || ch == '[' || ch == '(')
+            {
+                stk.push(ch);
+
+            }
+            else
+            {
+                if(stk.isEmpty()) return false;
+
+                if(ch == '}' && stk.pop() != '{') return false;
+                else if(ch == ')' && stk.pop() != '(') return false;
+                else if(ch == ']' && stk.pop() != '[') return false;
+                
+            }
+        
+        }
+        return stk.isEmpty();
+    }
+}
